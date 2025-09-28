61c428793b712abb215fd60c4aff27f6621275ee
Jordy de Jong <jordy.dejong@gmail.com>
2025-09-28 20:43:45 +0200

    migrations on startup

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,6 @@
 using TodoApi.Extensions;
+using TodoApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,23 @@ builder.Services.AddApiDocumentation();
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
+
+// Apply migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate();
+        Console.WriteLine("Database migrations applied successfully.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error applying migrations: {ex.Message}");
+        // In production, you might want to fail startup if migrations fail
+        // throw;
+    }
+}
 
 app.ConfigurePipeline();
 
